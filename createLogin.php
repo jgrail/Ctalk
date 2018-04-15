35fa72b75805e4fb22f9fdc18239bac501bab7cb
jgrail <31673963+jgrail@users.noreply.github.com>
2018-04-14 21:13:43 -0700

    Update and rename createLogin.html to createLogin.php

--- createLogin.php
+++ createLogin.php
@@ -1,18 +1,17 @@
 
- <html>
- <link rel = "stylesheet"
-   type = "text/css"
-   href = "createLogin.css" />
+<html>
+<link rel = "stylesheet"
+type = "text/css"
+href = "createLogin.css" />
 
-<form action="verifyAccount.php" style="border:1px solid cyan">
+
   <div class="container">
     <h1>Sign Up for ClaremontTalk!</h1>
     <p>Please fill in this form to create an account. you'll then be able to see and post messages</p>
-    <hr>
-
+<form action="verifyAccount.php" method = "post" style="border:1px solid cyan">
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>
-   
+    
 
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
@@ -20,11 +19,23 @@
     <label for="psw-repeat"><b>Repeat Password</b></label>
     <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
 
+    <label for="school" ><b>School</b></label> <br>
+    <select class = "button" name = "school">
+      <option value="Pomona">Pomona</option>
+      <option value="CMC">CMC</option>
+      <option value="Pitzer">Pitzer</option>
+      <option value="Scripps">Scripps</option>
+      <option value="Mudd">Mudd</option>
+      <option value="KGI">KGI</option>
+      <option value="CGU">CGU</option>
+      <option value="CUC">CUC</option>
+    </select>
+
 
     <p>By creating an account you agree to our <a href="#" style="color:blue">Terms and conditions</a>.</p>
 
     <div class="clearfix">
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="submit" class="button">Sign Up</button>
     </div>
   </div>
 </form>
