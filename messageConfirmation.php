c3b8841678bef5c3aaf9d8e6d5b8d8dad8fb7f2a
Michele Pashby <34638238+mpashby@users.noreply.github.com>
2018-04-14 20:36:49 -0700

    Update messageConfirmation.php

--- messageConfirmation.php
+++ messageConfirmation.php
@@ -5,7 +5,8 @@
 <head>
 </head>
 <body>
-	<p>Your message has been recorded. It will be distributed once it is validated by the administrators.
+	<h3>Your Message has been Recorded</h3>
+	<p>Thank you for posting your message! It will be distributed on Claremont Talk once it is validated by our administrators.
 	</p>
 	<href a="board.php">View Message Board</href>
 </body>
