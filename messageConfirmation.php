01db699fb0cc69295931887ad7d1272f2f920611
Michele Pashby <34638238+mpashby@users.noreply.github.com>
2018-04-14 19:51:59 -0700

    Add files via upload

---
+++ messageConfirmation.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+	<p>Your message has been recorded. It will be distributed once it is validated by the administrators.
+	</p>
+	<href a="board.php">View Message Board</href>
+</body>
+</html>
