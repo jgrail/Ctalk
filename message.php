106dcf0e20cf392cb8c9918da6cb2ed0eb9607fb
Michele Pashby <34638238+mpashby@users.noreply.github.com>
2018-04-14 20:37:36 -0700

    Update message.php

--- message.php
+++ message.php
@@ -1,26 +1,9 @@
 <?php
-session_start();
-require 'ddl.sql';
 require 'dbconn.php';
-/*define('DBHOST',"localhost");
-define('DBNAME',"ClaremontTalk");
-define('DBUSER',"Claremont");
-define('DBPASS',"talk");
-
-$connection = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-
-if ($connection -> connect_error) {
-	$output = "<P>Unable to connect to database</p>".$connection -> connect_error;
-	exit($output);
-}
-
-$sql = "GRANT USAGE On *.* to ".DBUSER. '@localhost identified by ' . "'" . DBPASS . "'"; */
-	//echo $output;
 ?>
 <?php
 
 if (isSet($_POST["submit"])) { //submitted form
-	 //validate firstname
 	$message = ($_POST['message']);
 	$type = $_POST['subject'];
 	$title = $_POST['title'];
@@ -29,25 +12,24 @@ if (isSet($_POST["submit"])) { //submitted form
 	//add User ID into database
 	$insertMsg = $connection->prepare($query1);
 	if ($insertMsg = $connection ->prepare($query1)) {
-		$insertMsg -> bind_param('sss' $message, $type, $title);
+		$insertMsg -> bind_param('sss', $message, $type, $title);
 	} else {
 		die("Error".$connection ->error);
 	}
 	mysqli_stmt_execute($insertMsg);
   	mysqli_stmt_close($insertMsg);	
-
 ///////////////////////////
-
 }	
 ?>
 <!DOCTYPE html>
 <head>
 </head>
 <body>
-	<form method= 'post' action = "messageConfirmation.php">
-		Title:
-		<input type = "text" name = "title" required>
-		Subject:
+	<h3>Please Enter Your Message</h3>
+	<form method= 'post' action='messageConfirmation.php' onsubmit= 'messageConfirmation.php'>
+		<strong>Title:</strong><br>
+		<input type = "text" name = "title" required><br><br>
+		<strong>Subject:</strong><br>
 		<select name = "subject">
 			<option value="tech">Technology</option>
 			<option value="science">Science</option>
@@ -55,11 +37,11 @@ if (isSet($_POST["submit"])) { //submitted form
 			<option value="food">Food</option>
 			<option value = "entertainment">Entertainment</option>
 			<option value = "other">Other</option>
-		</select>
-		Location:
-		<input type = "text" name = "location" required>
-		Message:
-		<input type = "text" name = "message" required>
+		</select><br><br>
+		<strong>Location:</strong><br>
+		<input type = "text" name = "location" required><br><br>
+		<strong>Message:</strong><br>
+		<input type = "text" name = "message" required><br>
 		<input type = "button" name = "img" value = "Upload Image">
 		<input type = "submit" value = "Submit Message">
 	</form>
