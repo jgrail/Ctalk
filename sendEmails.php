a323628e693867dc38bd416f297fb69a020d5b4b
jgrail <31673963+jgrail@users.noreply.github.com>
2018-04-24 21:16:38 -0700

    Update sendEmails.php
    Correct code to send an email

--- sendEmails.php
+++ sendEmails.php
@@ -1,1 +1,9 @@
-Send Emails Here
+<?php
+$to = "grail07@gmail.com";
+$subject = "CS Test";
+$txt = $message = file_get_contents("viewMessages.php");
+$headers = "From: claremont@talk.com";
+mail($to,$subject,$txt,$headers);
+echo $txt;
+
+?>
